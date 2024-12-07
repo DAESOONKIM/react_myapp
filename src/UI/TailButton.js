77b65854c3614c14b07df8537283c55b81d21777
DSKIM <gpdlqnd@gmail.com>
2024-12-07 17:58:06 +0900

    react_241707_useRef

--- src/UI/TailButton.js
+++ src/UI/TailButton.js
@@ -3,11 +3,13 @@ export default function TailButton({caption,bcolor,handleClick}) {
 
     const colorB = {
         'blue' : 'bg-blue-600',
-        'orange' : 'bg-orange-600'
+        'orange' : 'bg-orange-600',
+        'lime' : 'bg-lime-600'
     }
     const colorBHover = {
         'blue' : 'hover:bg-blue-800',
-        'orange' : 'hover:bg-orange-800'
+        'orange' : 'hover:bg-orange-800',
+         'lime' : 'hover:bg-lime-800'
     }
     return (
     <button className={`inline-flex px-5 py-3
