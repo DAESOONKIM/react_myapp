fae34b46a00a2508fe41f272dc94d0625796ee90
gpdlqnd <gpdlqnd@gmail.com>
2024-12-13 18:06:40 +0900

    react_route_20241213

---
+++ src/09/RoutePage1.js
@@ -0,0 +1,20 @@
+import { useParams } from "react-router-dom"
+
+
+export default function RoutePage1() {
+  const item = useParams().item;
+  const fruits = ['🍎','🍌'];
+  return (
+    <div className="w-full flex flex-col justify-center items-center">
+        <h1 className="text-2xl font-bold">
+            RoutePage1
+        </h1>
+        <div className="w-full flex flex-col justify-center items-center
+                        text-xl m02 p-2">
+            {item}
+            {fruits.includes(item) ?  ' : 과일입니다.' 
+                                    : ' : 과일이 아닙니다.'}                 
+        </div>
+    </div>
+  )
+}
--- src/ReTest.js
+++ src/ReTest.js
@@ -1,21 +1,21 @@
-import Header from './component/Header.js';
-import DayList from './component/DayList.js';
-import Day from './component/Day.js';
-import {BrowserRouter, Route, Switch } from 'react-router-dom'
+// import Header from './component/Header.js';
+// import DayList from './component/DayList.js';
+// import Day from './component/Day.js';
+// import {BrowserRouter, Route, Switch } from 'react-router-dom'
 
-function ReTest() {
+// function ReTest() {
 
-    return(
-     <BrowserRouter>
-        <div className='ReTest'>    
-            <Header/>
-            <Switch>
-                <Route  exact path="/"><DayList/></Route>
-                <Route  path="/day/:day"><Day/></Route>
-            </Switch>
-        </div>   
-    </BrowserRouter>
-    );
-}
+//     return(
+//      <BrowserRouter>
+//         <div className='ReTest'>    
+//             <Header/>
+//             <Switch>
+//                 <Route  exact path="/"><DayList/></Route>
+//                 <Route  path="/day/:day"><Day/></Route>
+//             </Switch>
+//         </div>   
+//     </BrowserRouter>
+//     );
+// }
 
-export default ReTest;
+// export default ReTest;
